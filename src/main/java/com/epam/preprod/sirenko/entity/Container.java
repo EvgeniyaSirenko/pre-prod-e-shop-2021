e61b40e077cb127ca9f9afba625c03b63670e1b1
EvgeniyaSirenko <sirenko.jane@gmail.com>
2021-08-20 16:09:37 +0300

    some methods of Container realized

--- src/main/java/com/epam/preprod/sirenko/entity/Container.java
+++ src/main/java/com/epam/preprod/sirenko/entity/Container.java
@@ -1,27 +1,92 @@
 package com.epam.preprod.sirenko.entity;
 
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Spliterator;
+import java.util.*;
 import java.util.function.UnaryOperator;
 
+/**
+ * Resizable array-implementation of the List interface
+ *
+ * @author E.Sirenko
+ *
+ **/
 public class Container implements List {
+	private int size = 10;
+	private Object[] array;
 	
+	public Container() {
+	}
+	
+	public Container(int length) {
+		this.array = new Object[length];
+	}
+	
+	/**
+	 *
+	 * Returns the number of elements
+	 **/
 	@Override
 	public int size() {
-		return 0;
+		return size;
 	}
 	
+	/**
+	 *
+	 * Removes all the elements
+	 **/
+	@Override
+	public void clear() {
+		for (int i = 0; i < size; i++)
+			array[i] = null;
+		size = 0;
+	}
+	
+	/**
+	 *
+	 * Appends the specified element to the end of the container
+	 **/
+	@Override
+	public boolean add(Object o) {
+		Object[] temp = array;
+		array = new Object[size + 1];
+		System.arraycopy(temp, 0, array, 0, size);
+		array[array.length - 1] = o;
+		size++;
+		return true;
+	}
+	
+	/**
+	 *
+	 * Inserts the specified element at the specified position
+	 **/
+	@Override
+	public void add(int index, Object o) {
+		Object[] temp = array;
+		array = new Object[size + 1];
+		System.arraycopy(temp, 0, array, 0, size);
+		array[index] = o;
+		size++;
+	}
+	
+	/**
+	 *
+	 * Returns true if container has no elements
+	 **/
 	@Override
 	public boolean isEmpty() {
-		return false;
+		return size == 0;
 	}
 	
+	/**
+	 *
+	 * Returns true if container has the specified element
+	 **/
 	@Override
 	public boolean contains(Object o) {
+		for (int i = 0; i < size; i++) {
+			if (array[i].equals(o)) {
+				return true;
+			}
+		}
 		return false;
 	}
 	
@@ -32,12 +97,7 @@ public class Container implements List {
 	
 	@Override
 	public Object[] toArray() {
-		return new Object[0];
-	}
-	
-	@Override
-	public boolean add(Object o) {
-		return false;
+		return null;
 	}
 	
 	@Override
@@ -62,28 +122,29 @@ public class Container implements List {
 	
 	@Override
 	public void sort(Comparator c) {
-	
+		Arrays.sort(array, 0, size, c);
 	}
 	
-	@Override
-	public void clear() {
 	
-	}
-	
+	/**
+	 *
+	 * Returns the element at the specified position
+	 **/
 	@Override
 	public Object get(int index) {
-		return null;
+		return array[index];
 	}
 	
+	/**
+	 *
+	 * Replaces the element at the specified position
+	 **/
 	@Override
 	public Object set(int index, Object element) {
-		return null;
+		array[index] = element;
+		return array[index];
 	}
-	
-	@Override
-	public void add(int index, Object element) {
-	
-	}
+
 	
 	@Override
 	public Object remove(int index) {
@@ -102,7 +163,7 @@ public class Container implements List {
 	
 	@Override
 	public Spliterator spliterator() {
-		return null;
+		return Spliterators.spliterator(array, Spliterator.ORDERED | Spliterator.IMMUTABLE);
 	}
 	
 	@Override
@@ -127,16 +188,19 @@ public class Container implements List {
 	
 	@Override
 	public ListIterator listIterator() {
+		//no realisation
 		return null;
 	}
 	
 	@Override
 	public ListIterator listIterator(int index) {
+		//no realisation
 		return null;
 	}
 	
 	@Override
 	public List subList(int fromIndex, int toIndex) {
+		//no realisation
 		return null;
 	}
 }
