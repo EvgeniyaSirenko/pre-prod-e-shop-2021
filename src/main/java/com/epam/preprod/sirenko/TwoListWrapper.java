4ec41da31cc8f3598d07a7bd8b7653c2ecc4d475
EvgeniyaSirenko <sirenko.jane@gmail.com>
2021-09-22 17:01:11 +0300

    final fields in iterator

--- src/main/java/com/epam/preprod/sirenko/TwoListWrapper.java
+++ src/main/java/com/epam/preprod/sirenko/TwoListWrapper.java
@@ -54,8 +54,8 @@ public class TwoListWrapper<E> implements List<E> {
 	 * Returns an iterator over the elements in both lists
 	 **/
 	private class TwoListIterator implements Iterator<E> {
-		private Iterator<E> unmodifiedListIterator;
-		private Iterator<E> modifiedListIterator;
+		private final Iterator<E> unmodifiedListIterator; //question: final has to be in upper case?
+		private final Iterator<E> modifiedListIterator;
 		
 		public TwoListIterator() {
 			this.unmodifiedListIterator = unmodifiedList.iterator();
